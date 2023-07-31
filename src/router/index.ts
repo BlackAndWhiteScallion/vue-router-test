de86d359aee5a2c3f143927c9f359c9e824d3ac2
BWS <tanguo@hotmail.com>
2023-07-31 11:10:40 +0800

    Initial commit

---
+++ src/router/index.ts
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+// @ts-ignore
+import loginview from '../views/loginview.vue'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  base: import.meta.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: loginview
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
+})
+
+export default router
